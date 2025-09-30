8be3388fce20c05f67a422a43f85ad7ff36bc539
Sherl0ck07 <imjadhavpr@gmail.com>
2025-09-30 10:01:51 +0530

    fresh-14

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 url = "https://graph.facebook.com/v22.0/729844620223276/messages"
-ACCESS_TOKEN = "EAAYtyB9hTPsBPtJDJlWIKVAUDZAUXedhocIFduHHGY3sbnns0yP9NGNGBS8A3Jz1wJn7c3YjunespGydPY2VezZAY8DuL1bAihZCvZCv0YXznUxw0USaZBfByrA3mzB8MtVxUgGff1FCZAjy38Ynj0dSZAzLotfKUNcswscOUBmNqNem2YsPRpryFQrxYFZB1YIupzwNcZClxkLwi67NH6l2whnDbF3qqYnyd0fMXSg4HZBfcaiAZDZD"  # from Meta App Dashboard
+ACCESS_TOKEN = "EAAYtyB9hTPsBPgNnzYp5d5fs2bLbbZAbJSrQVV5Ls25P4FJVtZB6Lw3I1rTVtePAnFBdBsnWVQNonfC63RkZByDRfgEiyPZCOri4cZC2AuDAZBm4vkCvIIN6sNCdclaYk8y8FeaBXBQQeJIwc3yVFhJdv2RTplgsoa8v3PaoHc0Cib95esRJ83ZAVoAZCuSZBErv1vxrdlrHAnVMYn81BR8ZAAOVrRd2FQike9KAiRvBZBHxOwOKuAZD"  # from Meta App Dashboard
 
 headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}",
@@ -11,7 +11,7 @@ headers = {
 
 payload = {
     "messaging_product": "whatsapp",
-    "to": "918459644404",  # recipient's WhatsApp number in international format
+    "to": "918484846888",  # recipient's WhatsApp number in international format
     "type": "template",
     "template": {
         "name": "hello_world",   # must exist in your WhatsApp template library
